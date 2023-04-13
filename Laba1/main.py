1cfd984eae728fe2a4eff5ac0979b48db866f6c0
Lolly384 <PKA2711@mail.ru>
2023-04-13 15:39:52 +0600

    Readme

--- Laba1/main.py
+++ Laba1/main.py
@@ -1,18 +1,5 @@
-from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup
-import requests # импортируем библиотеку requests
+import program
 
-def parse():
-    url = 'https://www.omgtu.ru/general_information/the-structure/the-department-of-university.php'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-    print(page.status_code)  # смотрим ответ
-    cafedry = []
-    for caf in soup.select('div#pagecontent > ul > li > a'):
-        cafedry.append(caf.text)
-
-    with open('cafedry.txt', 'w', encoding='utf-8') as f:
-        for caf in cafedry:
-            f.write(caf + '\n')
-
-
-parse()
+if __name__ == "__main__":
+    program.parse()
+    program.WriteFile()
