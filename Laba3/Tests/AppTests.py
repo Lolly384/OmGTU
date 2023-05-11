767d123719adb4aad5ddbb0f262917fabbf9a1a3
Lolly384 <PKA2711@mail.ru>
2023-05-11 21:57:53 +0600

    commit

--- Laba3/Tests/AppTests.py
+++ Laba3/Tests/AppTests.py
@@ -9,15 +9,25 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_convert(self):
+    def test_convert1(self):
         response = self.app.post('/convert', data={
             'amount': '10',
-            'from_currency': 'USD',
-            'to_currency': 'EUR'
+            'from_currency': 'EUR',
+            'to_currency': 'USD'
         })
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'10 USD =', response.data)
-        self.assertIn(b'EUR', response.data)
+        self.assertIn(b'10 EUR =', response.data)
+        self.assertIn(b'USD', response.data)
+
+    def test_convert2(self):
+        response = self.app.post('/convert', data={
+            'amount': '1',
+            'from_currency': 'USD',
+            'to_currency': 'RUB'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'1 USD =', response.data)
+        self.assertIn(b'RUB', response.data)
 
 if __name__ == '__main__':
     unittest.main()
